01b8a5499157d894acc11a31280122d792e7bf84
skatn <87296704+skatn@users.noreply.github.com>
2023-10-27 12:52:23 +0900

    Initial commit

---
+++ src/main/java/com/example/cicdpractice/CicdPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.cicdpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CicdPracticeApplication.class, args);
+    }
+
+}
