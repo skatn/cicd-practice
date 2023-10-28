76f1fe03e6fb470d8e5ebce7f007046b9ac31e2d
skatn <87296704+skatn@users.noreply.github.com>
2023-10-28 14:36:18 +0900

    file upload 기능 추가

--- src/main/java/com/example/cicdpractice/controller/HelloController.java
+++ src/main/java/com/example/cicdpractice/controller/HelloController.java
@@ -1,14 +1,29 @@
 package com.example.cicdpractice.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
 
 @RestController
-@RequestMapping("/hello")
 public class HelloController {
 
-    @RequestMapping
+//    private static final String FILE_DIR = "D:\\workspace\\yogiyo_team_project\\upload\\";
+    private static final String FILE_DIR = "~/image-store";
+
+    @RequestMapping("/hello")
     public String hello() {
         return "hello";
+    }
+
+    @PostMapping("/upload")
+    public String upload(MultipartFile file) throws IOException {
+        String pathname = FILE_DIR + file.getOriginalFilename();
+        file.transferTo(new File(pathname));
+
+        return "Save to " + pathname;
     }
 }
